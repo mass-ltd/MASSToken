48e6841be48056e6db337dd5cb491b9cfa9237e5
Kurt Knudsen <Kurt@mass.cloud>
2017-08-09 18:21:44 +0000

    updating trading contract. removing redundant code.

--- contracts/trading/MASSContract.sol
+++ contracts/trading/MASSContract.sol
@@ -14,11 +14,6 @@ import "./SafeMath.sol";
 // Prevent transactions to unapproved addresses
 // Kill switch - liquidate all assets and send to token holders, effectively ending MASSTokens
 
-// MASSToken contract function reference.
-contract MASSToken {
-
-}
-
 // Token subcontract function reference.
 contract Token {
   uint256 public _totalSupply;
@@ -37,7 +32,6 @@ contract MASSContract {
     uint256 public totalEthereum = 0; // Keep track of the value of the pool.    
 
     // Address mappings.
-    mapping (string => address) exchanges; //Map exchange names to addresses: "bittrex-1" => "0x0cc2....."
     mapping (string => string) stakedWallets; //Map wallet names to addresses: "neoscoin-1" => "0x0dd5...."
     
     struct Coins {
@@ -60,16 +54,27 @@ contract MASSContract {
         uint256 startTime;
         bool sent;
     }
+
+    struct Exchanges {
+        string name;
+        address addr;
+        uint256 amount;
+        bool canSend;
+        uint256 startTime;
+        bool active;
+    }
     
-    mapping (address => Outgoing) outgoingAddresses;
     mapping (string => Pending) pendingStaked;
     mapping (address => Pending) pendingExchanges;
+    mapping (address => Exchanges) exchangeAddresses;
+    // TODO:
+    // add coins functionality, currently only adds (pushes) coin to array.
     Coins[] coins;
     
     // Events
-    event NewOutgoingAddress(address);
-    event UpdatedOutgoingAddress(address);
-    event NewExchange(string, address);
+    event NewExchangeAddress(string, address);
+    event AllowedExchangeAddress(address);
+    event UpdatedExchange(string, address, bool);
     event NewStakedWallet(string, string);
     event FundedExchange(string, address, uint256);
     event FundedStakedWallet(string, address, uint256);
@@ -93,11 +98,9 @@ contract MASSContract {
     }
     
     // Accept eth from the ICO.
-    // Only allow this once.
+    // This can only happen once since the MASSToken contract will only send once and end.
     function () payable {
       require (msg.sender == MASSToken); // Only accept eth from the ICO contract.
-      if (hasICOEth) throw;
-      hasICOEth = true;
       totalEthereum += msg.value;
     }
 
@@ -115,32 +118,39 @@ contract MASSContract {
       }
     }
 
-    // Add an outgoing address. Set the start time to the current blocktime.    
-    function addOutgoingAddress(address _addr) public onlyOwner {
-      outgoingAddresses[_addr] = Outgoing(_addr, 0, false, now);
-      NewOutgoingAddress(_addr);
-    }
-    
-    // Allow addresses to be sendable after 1 week.
-    function updateOutgoingAddress(address _addr) public onlyOwner {
-      if (now < outgoingAddresses[_addr].startTime + (7 days)) throw;
-      outgoingAddresses[_addr].canSend = true;
-      UpdatedOutgoingAddress(_addr);
-    }
-    
-    // Allow pending transactions to exchanges to go through after 1 week.
-    function updatePendingExchangeTransaction(address _addr) public onlyOwner {
-      if (now < pendingExchanges[_addr].startTime + (7 days)) throw;
-      if (pendingExchanges[_addr].sent) throw; // Already sent.
-      pendingExchanges[_addr].sent = true;
-      _addr.transfer(pendingExchanges[_addr].amount);
-    }
-   
     // Add an address for an exchange. Set the start time to the current blocktime.
     function addExchangeAddress(string _name, address _addr) public onlyOwner {
-      exchanges[_name] = _addr;
-      outgoingAddresses[_addr] = Outgoing(_addr, 0, false, now);
-      NewExchange(_name, _addr);
+      exchangeAddresses[_addr] = Exchanges(_name, _addr, 0, false, now, true);
+      NewExchangeAddress(_name, _addr);
+    }
+
+    // Allow addresses to be sendable after 1 week.
+    function updateExchangeAddress(address _addr) public onlyOwner returns (bool success) {
+      if (now < exchangeAddresses[_addr].startTime + (7 days)) return false;
+      exchangeAddresses[_addr].canSend = true;
+      AllowedExchangeAddress(_addr);
+      return true;
+    }
+
+    // Allow pending transactions to exchanges to go through after 1 week.
+    function updatePendingExchangeTransaction(address _addr) public onlyOwner returns (bool success) {
+      if (now < pendingExchanges[_addr].startTime + (7 days)) return false;
+      if (pendingExchanges[_addr].sent) return false; // Already sent.
+      pendingExchanges[_addr].sent = true;
+      _addr.transfer(pendingExchanges[_addr].amount);
+      return true;
+    }
+   
+    // Deactivate an exchange address.
+    function removeExchangeAddress(string _name, address _addr) public onlyOwner {
+      exchangeAddresses[_addr].active = false;
+      UpdatedExchange(_name, _addr, false);
+    }
+
+    // Activate an exchange address that was previously deactivated. This cannot be used to bypass the waiting period.
+    function activateExchangeAddress(string _name, address _addr) public onlyOwner {
+      exchangeAddresses[_addr].active = true;
+      UpdatedExchange(_name, _addr, true);
     }
     
     // Add the name and address of a staking wallet.
@@ -150,21 +160,23 @@ contract MASSContract {
     }
     
     // Prepare to send eth to an exchange.
-    function fundExchange(string _name, address _addr, uint256 _amount) public onlyOwner {
-      if (!outgoingAddresses[_addr].canSend) throw; //Can only send to approved addresses.
-      if (exchanges[_name] != _addr) throw; //Make sure the address exists in our mapping.
+    function fundExchange(string _name, address _addr, uint256 _amount) public onlyOwner returns (bool success){
+      require(exchangeAddresses[_addr].canSend); //Can only send to approved addresses.
         
       // Calculate 30% of the pool's balance.
       uint256 maxSend = totalEthereum.mul(3);
       maxSend = maxSend.div(10);
-      if (maxSend < outgoingAddresses[_addr].amount) { //Do not send more than 30%.
+      if (maxSend < exchangeAddresses[_addr].amount) { //Do not send more than 30%.
         pendingExchanges[_addr] = Pending(_name, _addr, _amount, now, false);
         PendingExchange(_name, _addr, _amount);
+        return true;
       } else {
         _addr.transfer(_amount);
-        outgoingAddresses[_addr].amount = _amount;
+        exchangeAddresses[_addr].amount = _amount;
         FundedExchange(_name, _addr, _amount);
+        return true;
       }
+      return false; // This should not happen.
     }
     
     function addNewCoin(string name, string symbol, string url) public onlyOwner {
@@ -189,9 +201,10 @@ contract MASSContract {
     }
     
     // This function ends MASS and marks it so every token holder gets their fair share of Eth depending on how much MASS they hold.
-    function liquidateAll() public onlyOwner {
-      if (isLiquidated) throw;
+    function liquidateAll() public onlyOwner returns (bool success) {
+      if (isLiquidated) return false;
       isLiquidated = true;
       Liquidate();
+      return true;
     }
 }
